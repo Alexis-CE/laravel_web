39eba3ffe4256bda90e33154a99f16d4ea60528b
Alexis <2020pomelo@gmail.com>
2025-08-06 20:05:18 -0600

    12th

--- app/Models/Question.php
+++ app/Models/Question.php
@@ -22,12 +22,31 @@ class Question extends Model
 
     public function comments()
     {
-        return $this->morphMany(Comment::class, 'commentable');
+        return $this->morphMany(Comment::class, 'commentable'); // able
     }
 
     public function user()
     {
         return $this->belongsTo(User::class);
     }
-    
+
+    public function hearts()
+    {
+        return $this->morphMany(Heart::class, 'heartable');
+    }
+
+    public function isHearted()
+    {
+        return $this->hearts()->where('user_id', 20)->exists();
+    }
+
+    public function heart()
+    {
+        $this->hearts()->create(['user_id' => 20]);
+    }
+
+    public function unheart()
+    {
+        $this->hearts()->where('user_id', 20)->delete();
+    }
 }

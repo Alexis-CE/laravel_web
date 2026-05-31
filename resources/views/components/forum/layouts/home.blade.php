1df9eb24b19563a1492a6e0925735743954894fd
root <root@localhost.localdomain>
2026-05-31 10:19:56 -0600

    fix: assets vite, seeders, app config

--- resources/views/components/forum/layouts/home.blade.php
+++ resources/views/components/forum/layouts/home.blade.php
@@ -3,7 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Foro de programación</title>
+    <title>Codelab - Foro de programación</title>
+<meta name="description" content="Resuelve tus preguntas de programación. Comparte, aprende y crece con nuestra comunidad."><title>Foro de programación</title>
 
 <script>
     localStorage.removeItem('flux-appearance');
